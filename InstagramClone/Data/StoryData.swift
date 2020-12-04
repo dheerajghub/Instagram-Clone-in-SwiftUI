5a4f183ac87de35b7e52e481c9a4aa8112cfcc39
dheeraj <dheerajsh123456@gmail.com>
2020-12-04 20:09:07 +0530

    Final Push!

--- InstagramClone/Data/StoryData.swift
+++ InstagramClone/Data/StoryData.swift
@@ -9,8 +9,8 @@ import Foundation
 
 var StoryData:[StoryDataModel] = [
     StoryDataModel(image: "demo", name: "Your Story", isSeen: false, myStory:true ,myStoryActive: false),
-    StoryDataModel(image: "demo", name: "Your Story", isSeen: false, myStory:false ,myStoryActive: true),
-    StoryDataModel(image: "demo", name: "Your Story", isSeen: false, myStory:false ,myStoryActive: true),
-    StoryDataModel(image: "demo", name: "Your Story", isSeen: true, myStory:false ,myStoryActive: true),
-    StoryDataModel(image: "demo", name: "Your Story", isSeen: false, myStory:false ,myStoryActive: true)
+    StoryDataModel(image: "user1", name: "shubham_iosdev", isSeen: false, myStory:false ,myStoryActive: false),
+    StoryDataModel(image: "user2", name: "martinsoft", isSeen: false, myStory:false ,myStoryActive: false),
+    StoryDataModel(image: "user3", name: "edouard_iosdev", isSeen: false, myStory:false ,myStoryActive: false),
+    StoryDataModel(image: "user4", name: "ananjai04", isSeen: true, myStory:false ,myStoryActive: false),
 ]
